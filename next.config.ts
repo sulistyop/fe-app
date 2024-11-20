2d498f8d4cc672d31995b3f82659f887b3afc8c2
sulistyp <sulisdeveloper@gmail.,com>
2024-11-20 08:48:01 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Mengubah output menjadi file statis untuk GitHub Pages
+  basePath: '/fe-app', // Ganti <your-repository> dengan nama repository Anda
+  assetPrefix: '/fe-app', // Ganti <your-repository> dengan nama repository Anda
   images: {
-    domains: ['placeimg.com'],
+    domains: ['placeimg.com'], // Domain untuk memuat gambar eksternal
+    unoptimized: true, // Diperlukan untuk output statis
   },
 };
 
